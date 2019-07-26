7604f784061794c10ba93a4ee64ea01eacfd7a10
mdurban <mike.david.urban@gmail.com>
2019-07-25 22:51:06 -0400

    add contact page placeholder; fix navbar separator color

---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => (
+  <Layout>
+    <SEO title="about" />
+    <h1>Contact</h1>
+    <p>
+      Please don't contact me.
+    </p>
+  </Layout>
+)
+
+export default About
