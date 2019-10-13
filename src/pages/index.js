aedba16b611aa2ea49f61816162d1ba0ec4a87bd
mdurban <mike.david.urban@gmail.com>
2019-10-12 22:09:56 -0400

    fix footer font colors; remove duplicate footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,6 @@ const IndexPage = ({
         edges.map((edge, key) => blogPreview(edge, key))
       }
     </div>
-    <div className='footer'><Link className='footer-link' to="/disclaimer/">Disclaimer and Privacy Policy</Link></div>
   </Layout>
 }
 
