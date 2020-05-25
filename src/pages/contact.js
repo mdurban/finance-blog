b69f219bc552bfcaff23beb2fd684b82a78e95ce
mdurban <mike.david.urban@gmail.com>
2020-05-24 21:11:36 -0400

    finally version 1.0.0; clean up console warnings

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,12 +9,17 @@ const Contact = () => (
     <div className='contact-container'>
       <h1 className='contact-title'>Contact</h1>
       <form id='contact-form'>
-        <input className='email-form email-subject' type='text' placeholder='SUBJECT'></input>
-        <textarea className='email-form email-message' placeholder='MESSAGE' rows='8' cols='50'></textarea>
-        <button className='email-submit' type="button" onClick={() => {
-          alert('Thank you for contacting The Money Noob!')
-          document.getElementById('contact-form').reset();
-        }}>
+        <input className='email-form email-subject' type='text' placeholder='SUBJECT' />
+        <textarea className='email-form email-message' placeholder='MESSAGE' rows='8' cols='50' />
+        <button
+          className='email-submit'
+          type="button"
+          aria-label="submit"
+          onClick={() => {
+            alert('Thank you for contacting The Money Noob!')
+            document.getElementById('contact-form').reset();
+          }}
+        >
           Submit
       </button>
       </form>
--- src/pages/glossary.js
+++ src/pages/glossary.js
@@ -4,22 +4,20 @@ import Layout from '../components/layout';
 import SEO from "../components/seo"
 import { glossaryEntries } from '../helpers/glossaryEntries';
 const Glossary = () => (
-    <Layout>
-        <SEO title="glossary" />
-        <div className='glossary-container'>
-            <h1 className='glossary-title'>Glossary of Terms</h1>
-            {
-                glossaryEntries().map((glossaryKeyValue, index) => (
-                    <>
-                        <div key={index} className='glossary-entry'>
-                            <span className='entry-name'>{glossaryKeyValue.entry}</span>
-                            <span className='entry-definition'> - {glossaryKeyValue.definition}</span>
-                        </div>
-                    </>
-                ))
-            }
-        </div>
-    </Layout>
+  <Layout>
+    <SEO title="glossary" />
+    <div className='glossary-container'>
+      <h1 className='glossary-title'>Glossary of Terms</h1>
+      {
+        glossaryEntries().map((glossaryKeyValue, index) => (
+          <div key={index} className='glossary-entry'>
+            <span className='entry-name'>{glossaryKeyValue.entry}</span>
+            <span className='entry-definition'> - {glossaryKeyValue.definition}</span>
+          </div>
+        ))
+      }
+    </div>
+  </Layout>
 )
 
 export default Glossary
